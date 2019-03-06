db1c6d0bee9a9731ab2609ac00a66ee6135dc141
dzq <48273270+dongzengqiang@users.noreply.github.com>
2019-03-06 19:35:10 +0800

    Update SpringcloudEurekaApplication.java

--- src/main/java/com/jk/SpringcloudEurekaApplication.java
+++ src/main/java/com/jk/SpringcloudEurekaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//垃圾
 @SpringBootApplication
 @EnableEurekaServer
 public class SpringcloudEurekaApplication {
